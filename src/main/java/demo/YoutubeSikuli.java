710f041c11a8172c74d2cf56e1fe9dd8dfc2686a
AlkeshSakhare <akleshsakhare@yahoo.com>
2022-12-23 16:32:16 +0530

    edgebrowser

--- src/main/java/demo/YoutubeSikuli.java
+++ src/main/java/demo/YoutubeSikuli.java
@@ -1,7 +1,7 @@
 package demo;
 
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.edge.EdgeDriver;
 import org.sikuli.script.FindFailed;
 import org.sikuli.script.Pattern;
 import org.sikuli.script.Screen;
@@ -11,32 +11,32 @@ import io.github.bonigarcia.wdm.WebDriverManager;
 public class YoutubeSikuli {
 
 	public static void main(String[] args) throws FindFailed {
-		WebDriverManager.chromedriver().setup();
-		WebDriver driver = new ChromeDriver();
+		WebDriverManager.edgedriver().setup();
+		WebDriver driver = new EdgeDriver();
 		driver.manage().window().maximize();
 		driver.get("https://www.youtube.com/watch?v=07R_gvjIjdA");
 
 		Screen screen = new Screen();
 
-		Pattern pause = new Pattern("D:\\Alkesh\\Course\\Adv Java\\TestSikuli\\pause.png");
+		Pattern pause = new Pattern("pause.png");
 		screen.wait(pause, 2000);
 		screen.click();
 		screen.click();
 
-		Pattern play = new Pattern("D:\\Alkesh\\Course\\Adv Java\\TestSikuli\\play.png");
+		Pattern play = new Pattern("play.png");
 		screen.wait(play, 2000);
 		screen.click();
 		screen.click();
 
-		Pattern settings = new Pattern("D:\\Alkesh\\Course\\Adv Java\\TestSikuli\\settings.png");
+		Pattern settings = new Pattern("settings.png");
 		screen.wait(settings, 2000);
 		screen.click();
 
-		Pattern Auto360 = new Pattern("D:\\Alkesh\\Course\\Adv Java\\TestSikuli\\Auto 360.png");
+		Pattern Auto360 = new Pattern("Auto 360.png");
 		screen.wait(Auto360, 2000);
 		screen.click();
 
-		Pattern Auto244 = new Pattern("D:\\Alkesh\\Course\\Adv Java\\TestSikuli\\244.png");
+		Pattern Auto244 = new Pattern("244.png");
 		screen.wait(Auto244, 2000);
 		screen.click();
 	}
